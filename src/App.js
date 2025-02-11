00091732f91d41d15378d7d16fcb1d9734e1957d
frnadin <fernandogutilla@hotmail.com>
2025-02-11 18:02:32 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from './componentes/Banner/Banner';
+import TextInput from './componentes/TextInput/TextInput';
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <TextInput label={"fmg"} placeHolder={"fmg"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/TextInput/TextInput.js
@@ -0,0 +1,13 @@
+
+
+
+const TextInput = ({label, placeHolder}) => {
+    return (
+        <div>
+            <label>{label}</label>
+            <input placeholder={placeHolder} />
+        </div>
+    )
+}
+
+export default TextInput
