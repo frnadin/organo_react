5b6ed0a73aba6b65a19b6629531da1e0bbcccf00
fernandin <fernandogutilla@hotmail.com>
2025-02-11 21:53:10 -0300

    Começando criar componente | card de input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Banner from './componentes/Banner/Banner';
 import TextInput from './componentes/TextInput/TextInput';
+import './App.css'
 
 function App() {
   return (
-    <div>
+    <div id='body'>
       <Banner />
-      <TextInput label={"fmg"} placeHolder={"fmg"}/>
+        <div id='card-input'>
+          <h1>Preencha os dados para criar o card do colaborador.</h1>
+          <TextInput label={"Nome"} placeHolder={"Digite seu nome"}/>
+          <TextInput label={"Cargo"} placeHolder={"Digite seu cargo"}/>
+          <TextInput label={"Imagem"} placeHolder={"Digite a url da imagem"}/>
+        </div>
     </div>
   );
 }
--- src/componentes/TextInput/TextInput.js
+++ src/componentes/TextInput/TextInput.js
@@ -1,10 +1,11 @@
 
-
+import './TextInput.css'
 
 const TextInput = ({label, placeHolder}) => {
     return (
-        <div>
+        <div className="input-card">
             <label>{label}</label>
+            <br/>
             <input placeholder={placeHolder} />
         </div>
     )
